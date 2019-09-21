156cf773ca25e03e4c9ee58a7640a96cde20933c
Prakhar Mittal <prakhar.mittal@moonraft.com>
2019-09-21 23:07:59 +0530

    Updates

---
+++ precache-manifest.9a0d864fb4f56539a3d72d4daedde44b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d085dfebd5c2a924b70cf4f7482bd0",
+    "url": "/Album-Carousel/index.html"
+  },
+  {
+    "revision": "bb8cc6e1f2adefba1112",
+    "url": "/Album-Carousel/static/css/main.a0fa9a84.chunk.css"
+  },
+  {
+    "revision": "7e3c6e396297a4d2719f",
+    "url": "/Album-Carousel/static/js/2.cc502190.chunk.js"
+  },
+  {
+    "revision": "bb8cc6e1f2adefba1112",
+    "url": "/Album-Carousel/static/js/main.7e811009.chunk.js"
+  },
+  {
+    "revision": "881a8f4f721a6716e3bd",
+    "url": "/Album-Carousel/static/js/runtime~main.009527c4.js"
+  }
+]);
