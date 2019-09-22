56d0afffc0c6ec6b257de74be1525abdde82ad9f
Prakhar Mittal <prakhar.mittal@moonraft.com>
2019-09-22 17:09:10 +0530

    using all API's

--- src/components/Slider/AlbumSlider.js
+++ src/components/Slider/AlbumSlider.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Axios from "axios";
-import Slider from './Slider';
+import Carousel from "../../Carousel/Carousel";
+import { LazyImageProvider } from "../../LazyImage/LazyImageContext";
+import LazyImage from "../../LazyImage/LazyImage"; // Import LazyImage
 
 export default class AlbumSlider extends Component {
     state = {
         imagesArr: []
     }
-    componentWillMount() {
+    componentDidMount() {
         Axios.get(`https://jsonplaceholder.typicode.com/photos?albumId=${this.props.id}`).then(
             response => {
                 this.setState({ imagesArr: response.data });
@@ -15,9 +17,20 @@ export default class AlbumSlider extends Component {
     }
     render() {
         const { imagesArr } = this.state;
-        if (imagesArr == []) return <div>Loading...</div>;
+        if (!imagesArr.length)
+            return null;
         return (
-            <Slider images={imagesArr} />
+            <LazyImageProvider>
+                <Carousel>
+                    {imagesArr.map((image, index) => (
+                        <>
+                            <LazyImage aspectRatio={[10, 7]} src={image.url} key={index} />
+                            <div className="imageTitle">{image.title}</div>
+                            <div className="imageId">id: {image.id}</div>
+                        </>
+                    ))}
+                </Carousel>
+            </LazyImageProvider>
         )
     }
 }
